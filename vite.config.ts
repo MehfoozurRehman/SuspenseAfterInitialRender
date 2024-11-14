6419d6cfccee014579c3a9820b87e5e3b67ff349
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-14 23:55:12 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,26 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression'
+import compression from "vite-plugin-compression";
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.tsx'),
-      name: 'SuspenseAfterInitialRender',
-      fileName: (format) => `suspense-after-initial-render.${format}.js`
+      entry: path.resolve(__dirname, "index.tsx"),
+      name: "SuspenseAfterInitialRender",
+      fileName: (format) => `suspense-after-initial-render.${format}.js`,
     },
     rollupOptions: {
-      // externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react', 'react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React'
-        }
-      }
-    }
+      external: ["react", "react-dom"],
+      output: { globals: { react: "React" } },
+    },
   },
   plugins: [
     react(),
     compression({
       algorithm: "brotliCompress",
-      threshold: 100
-    })
-  ]
-})
+      threshold: 100,
+    }),
+  ],
+});
